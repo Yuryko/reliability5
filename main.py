267167011de3a238d02e7e8b124d1fdc5371ae02
Юрий Козлов <kozlovye@gmail.com>
2021-06-24 10:19:28 +0300

    Работа с графиками

--- main.py
+++ main.py
@@ -267,38 +267,38 @@ plt.show()
 
 '''
 
-fig = plt.figure(figsize=(8, 8))
-ax = fig.add_subplot(1, 1, 1, aspect=1)
+fig = plt.figure(figsize=(9, 9))
+ax = fig.add_subplot(1, 1, 1, aspect=90000)
 
 def minor_tick(x, pos):
     if not x % 1.0:
         return ""
     return "%.2f" % x
 
-ax.xaxis.set_major_locator(MultipleLocator(1.000))
+ax.xaxis.set_major_locator(MultipleLocator(9000.000))
 ax.xaxis.set_minor_locator(AutoMinorLocator(4))
-ax.yaxis.set_major_locator(MultipleLocator(1.000))
-ax.yaxis.set_minor_locator(AutoMinorLocator(4))
-ax.xaxis.set_minor_formatter(FuncFormatter(minor_tick))
+ax.yaxis.set_major_locator(MultipleLocator(0.1))
+ax.yaxis.set_minor_locator(AutoMinorLocator(1))
+# ax.xaxis.set_minor_formatter(FuncFormatter(minor_tick))
 
-ax.set_xlim(0, 4)
-ax.set_ylim(0, 4)
+ax.set_xlim(0, 90000)
+ax.set_ylim(0.5, 1.1)
 
 ax.tick_params(which='major', width=1.0)
-ax.tick_params(which='major', length=10)
-ax.tick_params(which='minor', width=1.0, labelsize=10)
-ax.tick_params(which='minor', length=5, labelsize=10, labelcolor='0.25')
+ax.tick_params(which='major', length=1.0)
+ax.tick_params(which='minor', width=1.0, labelsize=1.0)
+# ax.tick_params(which='minor', length=5, labelsize=10, labelcolor='0.25')
 
 ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
 
-ax.plot(T, vbr, c=(0.25, 0.25, 1.00), lw=2, label="Blue signal", zorder=10)
-ax.plot(T, vbr1, c=(1.00, 0.25, 0.25), lw=2, label="Red signal")
+ax.plot(T, vbr, lw=2, label=u"Без мажорирования")  # ax.plot(T, vbr, c=(0.25, 0.25, 1.00), lw=2, label="Blue signal", zorder=10)
+ax.plot(T, vbr1, lw=2, label=u"С мажорированием") # ax.plot(T, vbr1, c=(1.00, 0.25, 0.25), lw=2, label="Red signal")
 # ax.plot(X, Y3, linewidth=0,
 #        marker='o', markerfacecolor='w', markeredgecolor='k')
 
-ax.set_title('<- Надежность ->', fontsize=20, verticalalignment='bottom')
-ax.set_xlabel("X axis label")
-ax.set_ylabel("Y axis label")
+ax.set_title(u"Вероятность безотказной работы", fontsize=20, verticalalignment='bottom')
+ax.set_xlabel(u"Время работы (ч)")
+ax.set_ylabel(u"Вероятность")
 
 ax.legend()
 
@@ -315,6 +315,118 @@ def circle(x, y, radius=0.15):
 def text(x, y, text):
     ax.text(x, y, text, backgroundcolor="white",
             ha='center', va='top', weight='bold', color='blue')
+
+'''
+тут всякие кружочки
+# Minor tick
+# circle(0.50, -0.10)
+# text(0.50, -0.32, "Minor tick label")
+
+# Major tick
+circle(-0.03, 4.00)
+text(0.03, 3.80, "Major tick")
+
+# Minor tick
+circle(0.00, 3.50)
+text(0.00, 3.30, "Minor tick")
+
+# Major tick label
+circle(-0.15, 3.00)
+text(-0.15, 2.80, "Major tick label")
+
+# X Label
+circle(1.80, -0.27)
+text(1.80, -0.45, "X axis label")
+
+# Y Label
+circle(-0.27, 1.80)
+text(-0.27, 1.6, "Y axis label")
+
+# Title
+circle(1.60, 4.13)
+text(1.60, 3.93, "Title")
+
+# Blue plot
+circle(1.75, 2.80)
+text(1.75, 2.60, "Line\n(line plot)")
+
+# Red plot
+circle(1.20, 0.60)
+text(1.20, 0.40, "Line\n(line plot)")
+
+# Scatter plot
+circle(3.20, 1.75)
+text(3.20, 1.55, "Markers\n(scatter plot)")
+
+# Grid
+circle(3.00, 3.00)
+text(3.00, 2.80, "Grid")
+
+# Legend
+circle(3.70, 3.80)
+text(3.70, 3.60, "Legend")
+
+# Axes
+circle(0.5, 0.5)
+text(0.5, 0.3, "Axes")
+
+# Figure
+circle(-0.3, 0.65)
+text(-0.3, 0.45, "Figure")
+
+
+color = 'blue'
+ax.annotate('Spines', xy=(4.0, 0.35), xycoords='data',
+            xytext=(3.3, 0.5), textcoords='data',
+            weight='bold', color=color,
+            arrowprops=dict(arrowstyle='->',
+                            connectionstyle="arc3",
+                            color=color))
+
+ax.annotate('', xy=(3.15, 0.0), xycoords='data',
+            xytext=(3.45, 0.45), textcoords='data',
+            weight='bold', color=color,
+            arrowprops=dict(arrowstyle='->',
+                            connectionstyle="arc3",
+                            color=color))
+'''
+
+ax.text(4.0, -0.4, "(JSC) Scientific Research Institute For Watch Industry",
+        fontsize=10, ha="right", color='.5')
+
+plt.show()
+
+# отображение отказа
+fig1 = plt.figure(figsize=(9, 9))
+ax = fig1.add_subplot(1, 1, 1, aspect=90000)
+
+ax.xaxis.set_major_locator(MultipleLocator(9000.000))
+ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+ax.yaxis.set_major_locator(MultipleLocator(0.1))
+ax.yaxis.set_minor_locator(AutoMinorLocator(1))
+# ax.xaxis.set_minor_formatter(FuncFormatter(minor_tick))
+
+ax.set_xlim(0, 90000)
+ax.set_ylim(0.5, 1.1)
+
+ax.tick_params(which='major', width=1.0)
+ax.tick_params(which='major', length=1.0)
+ax.tick_params(which='minor', width=1.0, labelsize=1.0)
+# ax.tick_params(which='minor', length=5, labelsize=10, labelcolor='0.25')
+
+ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
+
+ax.plot(T, vbr, lw=2, label=u"Без мажорирования")  # ax.plot(T, vbr, c=(0.25, 0.25, 1.00), lw=2, label="Blue signal", zorder=10)
+ax.plot(T, vbr1, lw=2, label=u"С мажорированием") # ax.plot(T, vbr1, c=(1.00, 0.25, 0.25), lw=2, label="Red signal")
+# ax.plot(X, Y3, linewidth=0,
+#        marker='o', markerfacecolor='w', markeredgecolor='k')
+
+ax.set_title(u"Вероятность безотказной работы \n с моделированием отказа", fontsize=20, verticalalignment='bottom')
+ax.set_xlabel(u"Время работы (ч)")
+ax.set_ylabel(u"Вероятность")
+
+ax.legend()
+
 
 '''
 тут всякие кружочки
